f8dc42169640887b058fd4c2d820a1ca3cbda10a
Alesia6 <alesiaalikaj6@gmail.com>
2025-09-25 11:54:30 +0200

    dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import  { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+export const routes: Routes = [
+{ path: '', component: LoginComponent },     
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
+
---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent implements OnInit {
+balance: number = 0;
+accountId: number = 0;
+
+constructor(
+  private apiService: ApiService,
+  private authService: AuthService
+){}
+
+ngOnInit(): void {
+this.accountId = this.authService.getAccountId() || 0;
+this.loadBalance();
+}
+
+loadBalance(): void {
+  this.apiService.getBalance().subscribe({
+    next: (response) => {
+      this.balance = response.balance;
+    },
+    error: (error) => {
+      console.error("Failed to load balance:", error)
+    }
+  });
+}
+
+logout(): void {
+  this.authService.logout();
+}
+}
