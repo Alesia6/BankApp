d7514245051f527098ae0f7519c7c1b71e0c7079
Alesia6 <alesiaalikaj6@gmail.com>
2025-10-02 11:07:08 +0200

    transaction histroy component

---
+++ src/app/components/transactions/transactions.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Transaction } from '../../models/transaction';
+import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-transactions',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './transactions.component.html',
+  styleUrl: './transactions.component.css'
+})
+export class TransactionsComponent implements OnInit {
+
+  transactions: Transaction[] =[];
+  limit: number=50;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+    }
+ loadTransactions(limit?: number): void {
+  if (limit) this.limit = limit;
+
+  this.apiService.getTransactions(this.limit).subscribe({
+    next: (transactions) => {
+      this.transactions = transactions;
+    },
+    error: (error) => {
+     console.log("Failed to load transactions:", error);
+    },
+    
+  });
+ }
+ getTransactionClass(type: string): string {
+  return type.toLowerCase();
+ }
+
+  } 
